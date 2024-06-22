9416d4d1d7cfea10b5f5c20be68445e940842c2f
Mr WANG <2362999724@qq.com>
2024-06-22 10:09:47 +0800

    add profile fragment

--- app/src/main/java/mob/godutch/easychat/ProfileFragment.java
+++ app/src/main/java/mob/godutch/easychat/ProfileFragment.java
@@ -1,5 +1,6 @@
 package mob.godutch.easychat;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,105 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import mob.godutch.easychat.model.UserModel;
+import mob.godutch.easychat.utils.AndroidUtil;
+import mob.godutch.easychat.utils.FirebaseUtil;
 
 public class ProfileFragment extends Fragment {
+    ImageView profilePic;
+    EditText usernameInput;
+    EditText phoneInput;
+    Button updateProfileBtn;
+    ProgressBar progressBar;
+    TextView logoutBtn;
+
+
+    UserModel currentUserModel;
+
 
     public ProfileFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profilePic = view.findViewById(R.id.profile_image_view);
+        usernameInput = view.findViewById(R.id.profile_username);
+        phoneInput = view.findViewById(R.id.profile_phone);
+        updateProfileBtn = view.findViewById(R.id.profile_update_btn);
+        progressBar = view.findViewById(R.id.profile_progress_bar);
+        logoutBtn = view.findViewById(R.id.logout_btn);
+
+        getUserData();
+        updateProfileBtn.setOnClickListener(v -> {
+            updateBtnClick();
+        });
+        logoutBtn.setOnClickListener(v -> {
+            FirebaseUtil.logout();
+            Intent intent = new Intent(getContext(), SplashActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
+        return view;
+    }
+
+    void updateBtnClick() {
+        String newUsername = usernameInput.getText().toString();
+        if (newUsername.isEmpty() || newUsername.length() < 3) {
+            usernameInput.setError("Username length should be at least 3 chars");
+            return;
+        }
+        currentUserModel.setUsername(newUsername);
+        updateToFirestore();
+    }
+
+    void updateToFirestore() {
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().set(currentUserModel)
+                .addOnCompleteListener(task -> {
+                    setInProgress(false);
+                    if (task.isSuccessful()) {
+                        AndroidUtil.showToast(getContext(), "Update successfully");
+                    } else {
+                        AndroidUtil.showToast(getContext(), "Update failed");
+                    }
+                });
+    }
+
+    void getUserData() {
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails()
+                .get()
+                .addOnCompleteListener(task -> {
+                    setInProgress(false);
+                    if (task.isSuccessful()) {
+                        currentUserModel = task.getResult().toObject(UserModel.class);
+                        if (currentUserModel != null) {
+                            usernameInput.setText(currentUserModel.getUsername());
+                            phoneInput.setText(currentUserModel.getPhone());
+
+                        }
+                    }
+                });
+    }
+
+    void setInProgress(boolean inProgress) {
+        if (inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            updateProfileBtn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            updateProfileBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
--- app/src/main/java/mob/godutch/easychat/utils/FirebaseUtil.java
+++ app/src/main/java/mob/godutch/easychat/utils/FirebaseUtil.java
@@ -50,4 +50,7 @@ public class FirebaseUtil {
     public static String timestampToString(Timestamp timestamp) {
         return new SimpleDateFormat("HH:MM").format(timestamp.toDate());
     }
+    public static void logout() {
+        FirebaseAuth.getInstance().signOut();
+    }
 }
